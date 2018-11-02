bae790a9ea1e62e21295fb38fc0ac4fa66aa49ea
China-zhuangxuxin <zhuangxuxin@hotmail.com>
2018-11-03 01:56:11 +0800

    init projects

---
+++ lichkin-projects-cashier-desk-admin-launcher/copy.bat
@@ -0,0 +1,4 @@
+copy /b ..\..\lichkin-projects-core\lichkin-projects-core-admin-launcher\src\main\resources\META-INF\resources\res\js\i18n\zh_CN.js+..\lichkin-projects-cashier-desk-admin-ui\src\main\resources\META-INF\resources\res\js\admin\cashier-desk\XxxMgmt\index\i18n\zh_CN.js zh_CN.js
+move zh_CN.js src\main\resources\META-INF\resources\res\js\i18n
+copy /b ..\..\lichkin-projects-core\lichkin-projects-core-admin-launcher\src\main\resources\META-INF\resources\res\js\icons.js+..\lichkin-projects-cashier-desk-admin-ui\src\main\resources\META-INF\resources\res\js\admin\cashier-desk\XxxMgmt\index\icons.js icons.js
+move icons.js src\main\resources\META-INF\resources\res\js
